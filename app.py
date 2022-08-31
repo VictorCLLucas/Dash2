defa171b421a048b9826317bec01c992305656b2
Victor Lucas <lucas.victor89@gmail.com>
2022-08-31 22:09:21 +0100

    changing return message

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def home():
           : :::::'        `                                                    
            ``
     """
-    return xmen
+    return ascii(xmen)
 
 
 if __name__ == "__main__":
