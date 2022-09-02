d839a3c29c957ba64c08b8a09e42dfa2f9330f57
Victor Lucas <lucas.victor89@gmail.com>
2022-09-02 15:45:59 +0100

    Adding external call to deliver boredApi to new endpoint

--- external_calls.py
+++ external_calls.py
@@ -1,7 +1,8 @@
 import urllib
+from urllib.request import urlopen
 
 def get_bored_data():
-	link = "http://http://www.boredapi.com/api/activity"
-	f = urllib.urlopen(link)
+	link = "http://www.boredapi.com/api/activity"
+	f = urlopen(link)
 	myfile = f.read()
 	return(myfile)
