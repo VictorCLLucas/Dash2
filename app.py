47749df6d01762e24e294fea870d3337196d87ff
Victor Lucas <lucas.victor89@gmail.com>
2022-09-16 12:07:46 +0100

    Auth0 on boredApi endpoint

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from crypt import methods
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api
+from flask_cors import cross_origin
 
 import external_calls as ext
-
+from server import requires_auth
 
 app = Flask(__name__)
 api = Api(app)
 
 @app.route('/', methods=['GET'])
+@cross_origin(headers=['Content-Type', 'Authorization'])
+@requires_auth
 def home():
 		resp = app.make_response('Hello World!')
 		resp.headers["Access-Control-Allow-Origin"] = "*"
@@ -16,6 +19,8 @@ def home():
 
 
 @app.route("/boredapi", methods=['GET'])
+@cross_origin(headers=['Content-Type', 'Authorization'])
+@requires_auth
 def get_bored_api():
 	bored_api_data = ext.get_bored_data()
 	resp = app.make_response(bored_api_data)
